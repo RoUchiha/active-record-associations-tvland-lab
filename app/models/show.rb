e370929007149062a33e7af63ca5fc1e90e52b6e
RoUchiha <swaggsikh@gmail.com>
2021-10-12 01:58:17 -0500

    commit tree dd597727a45b92a434bdd4ecf9cd99c35638993e

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
+
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        names = []
+        characters.each do |c|
+            names << c.actor.full_name
+        end
+        return names
+    end
   
 end
